70586e21fff6fc73915029eeb9ed7bdd9d770c85
NguyenPhuc <20521767@gm.uit.edu.vn>
2023-06-09 11:56:47 +0700

    NguyenPhuc - Complete responsive footer

--- src/components/main/Footer/footer.jsx
+++ src/components/main/Footer/footer.jsx
@@ -6,31 +6,31 @@ function Footer(props) {
     <footer>
       <div className="wrapper-footer">
           <div className="logo">TechShop</div>
-          <div className="row d-flex flex-[1376px]-row flex-col justify-content-center align-items-start">
-            <div className="col-md-4" style={{ marginRight: "1rem" }}>
-              <div className="header">
+          <div className="row d-flex flex-xl-row flex-col justify-content-center align-items-start">
+            <div className="col-xl-4" style={{ marginRight: "1rem", padding: "1rem" }}>
+              <div className="header text-nowrap">
                 <p>Service & Support</p>
               </div>
-              <div className="content">
+              <div className="content text-nowrap">
                 <p>techshop@gmail.com</p>
                 <p>0904122122</p>
               </div>
             </div>
-            <div className="col-md-3" style={{ marginRight: "1rem" }}>
-              <div className="header">
+            <div className="col-xl-3" style={{ marginRight: "1rem", padding: "1rem" }}>
+              <div className="header text-nowrap">
                 <p>Info</p>
               </div>
-              <div className="content">
+              <div className="content text-nowrap">
                 <p>Delivery & Returns</p>
                 <p>About Us</p>
                 <p>Payment Guide</p>
               </div>
             </div>
-            <div className="col-md-2">
-              <div className="header">
+            <div className="col-xl-2" style={{ padding: "1rem" }}>
+              <div className="header text-nowrap">
                 <p>Follow Us</p>
               </div>
-              <div className="content">
+              <div className="content text-nowrap">
                 <p>Facebook</p>
                 <p>Instagram</p>
               </div>
