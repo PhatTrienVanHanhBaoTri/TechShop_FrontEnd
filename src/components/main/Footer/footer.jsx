0b1049e60a55a2f08e1dfd45d6b434d383eecac1
NguyenPhuc <20521767@gm.uit.edu.vn>
2023-06-09 11:45:52 +0700

    responsive footer

--- src/components/main/Footer/footer.jsx
+++ src/components/main/Footer/footer.jsx
@@ -6,8 +6,8 @@ function Footer(props) {
     <footer>
       <div className="wrapper-footer">
           <div className="logo">TechShop</div>
-          <div className="row">
-            <div className="col-5">
+          <div className="row d-flex flex-[1376px]-row flex-col justify-content-center align-items-start">
+            <div className="col-md-4" style={{ marginRight: "1rem" }}>
               <div className="header">
                 <p>Service & Support</p>
               </div>
@@ -16,7 +16,7 @@ function Footer(props) {
                 <p>0904122122</p>
               </div>
             </div>
-            <div className="col-4">
+            <div className="col-md-3" style={{ marginRight: "1rem" }}>
               <div className="header">
                 <p>Info</p>
               </div>
@@ -26,7 +26,7 @@ function Footer(props) {
                 <p>Payment Guide</p>
               </div>
             </div>
-            <div className="col-3">
+            <div className="col-md-2">
               <div className="header">
                 <p>Follow Us</p>
               </div>
