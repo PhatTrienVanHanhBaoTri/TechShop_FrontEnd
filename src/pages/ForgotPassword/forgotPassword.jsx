2d508c63e9d8f7f8aa18f1610af0b4e53d92689a
blackie832 <quandang123654@gmail.com>
2023-07-08 00:19:30 +0700

    fix bugs

--- src/pages/ForgotPassword/forgotPassword.jsx
+++ src/pages/ForgotPassword/forgotPassword.jsx
@@ -37,8 +37,6 @@ export const ForgotPassword = () => {
       }
       submitToSendOTP();
       dispatch(updateEmail({ userEmail: email }));
-
-      // setLoading(true);
     }
   };
   return (
@@ -74,7 +72,7 @@ export const ForgotPassword = () => {
               >
                 Send OTP
               </button>
-              <button className="btn-secondary px-3 py-2">
+              <button className="btn-secondary px-3 py-2 mt-2">
                 <Link to="/login">Back to Login</Link>
               </button>
             </div>
